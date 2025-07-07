f04b0db56ccd696d8fb96beb8ce98ac95b7e081f
315tozu <sable870@yahoo.co.jp>
2025-07-07 13:38:33 +0900

    フォルダをアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const btnId = document.getElementById('btn');
+
+btnId.addEventListener('click', () =>{
+  const textWord = document.getElementById('text');
+  textWord.textContent = 'ボタンをクリックしました'
+});
